158194dccf901dea2ad471451f89e3d5d9c5d553
achilles687 <achilles687@gmail.com>
2023-06-26 22:00:05 +0300

    u

--- jss.js
+++ jss.js
@@ -1,4 +1,5 @@
 const keys = ["a", "s", "d", "f", "g", "h", "j", "k", "l", "ş", "o", "p", "w", "e", "t", "y", "u", ";"];
+let keyPlaying = {}; // Basılı tutulan tuşları depolamak için bir nesne
 
 function handleKeyDown(event) {
     const key = event.key.toLowerCase();
@@ -6,9 +7,12 @@ function handleKeyDown(event) {
     const keyElement = document.getElementById(`key${key.toUpperCase()}`);
 
     if (keys.includes(key)) {
+        if (keyPlaying[audioKey]) return; // Eğer tuş zaten basılı tutuluyorsa, tekrar çalmayı engelle
 
         const audio = new Audio(`audio/${audioKey}.wav`);
         audio.play();
+
+        keyPlaying[audioKey] = true; // Basılı tutulan tuşu işaretlemek için nesneye ekle
 
         keyElement.classList.add('flash');
         setTimeout(() => {
@@ -16,6 +20,50 @@ function handleKeyDown(event) {
         }, 200);
     }
 }
+
+function handleKeyUp(event) {
+    const key = event.key.toLowerCase();
+    const audioKey = (key === ';') ? 'ş' : key;
+
+    if (keys.includes(key)) {
+        keyPlaying[audioKey] = false; // Basılı tutulan tuşun işaretini kaldır
+    }
+}
+
+function playSound(key) {
+    if (keys.includes(key)) {
+        if (keyPlaying[key]) return;
+
+        const audio = new Audio(`audio/${key}.wav`);
+        audio.play();
+
+        keyPlaying[key] = true;
+
+        audio.addEventListener('ended', () => {
+            delete keyPlaying[key];
+        });
+    }
+}
+
+function handleMouseDown(event) {
+    const key = event.target.textContent.toLowerCase();
+    playSound(key);
+
+    event.target.classList.add('flash');
+    setTimeout(() => {
+        event.target.classList.remove('flash');
+    }, 200);
+}
+
+function handleMouseUp(event) {
+    const key = event.target.textContent.toLowerCase();
+
+    event.target.classList.remove('flash');
+    delete keyPlaying[key];
+}
+
+window.addEventListener('mousedown', handleMouseDown);
+window.addEventListener('mouseup', handleMouseUp);
 
 function showKeysByKeyboardLanguage() {
     const turkishKeys = document.getElementsByClassName("turkish");
@@ -33,6 +81,11 @@ function showKeysByKeyboardLanguage() {
 }
 
 document.addEventListener('keydown', handleKeyDown);
+document.addEventListener('keyup', handleKeyUp);
+const keysContainer = document.querySelector('.container');
+keysContainer.addEventListener('mousedown', handleMouseDown);
+keysContainer.addEventListener('mouseup', handleMouseUp);;
+
 document.addEventListener("DOMContentLoaded", showKeysByKeyboardLanguage);
 window.addEventListener("languagechange", showKeysByKeyboardLanguage);
 
